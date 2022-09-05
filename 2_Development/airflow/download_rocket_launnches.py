99428e5a7007353b121e4d25f9d81f52590f2469
Rolando Mirabal <ramirabal@gmail.com>
2022-09-05 17:56:06 -0400

    Create download_rocket_launnches.py
    Example curl request and response to the Launch Library API

---
+++ 2_Development/airflow/download_rocket_launnches.py
@@ -0,0 +1,34 @@
+$ curl -L "https://ll.thespacedevs.com/2.0.0/launch/upcoming"
+ 
+{
+ ...
+ "results": [
+   {
+     "id": "528b72ff-e47e-46a3-b7ad-23b2ffcec2f2",
+     "url": "https://.../528b72ff-e47e-46a3-b7ad-23b2ffcec2f2/",
+     "launch_library_id": 2103,
+     "name": "Falcon 9 Block 5 | NROL-108",
+     "net": "2020-12-19T14:00:00Z",
+     "window_end": "2020-12-19T17:00:00Z",
+     "window_start": "2020-12-19T14:00:00Z",
+     ➥ "image":
+     "https://spacelaunchnow-prod-east.nyc3.digitaloceanspaces.com/
+      media/launch_images/falcon2520925_image_20201217060406.jpeg",
+     "infographic": ".../falcon2520925_infographic_20201217162942.png",
+     ...
+   },
+   {
+     "id": "57c418cc-97ae-4d8e-b806-bb0e0345217f",
+     "url": "https://.../57c418cc-97ae-4d8e-b806-bb0e0345217f/",
+     "launch_library_id": null,
+     "name": "Long March 8  | XJY-7 & others",
+     "net": "2020-12-22T04:29:00Z",
+     "window_end": "2020-12-22T05:03:00Z",
+     "window_start": "2020-12-22T04:29:00Z",
+     "image": "https://.../long2520march_image_20201216110501.jpeg",
+     "infographic": null,
+     ...
+   },
+   ...
+ ]
+}
